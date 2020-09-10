e7b30853c5e01f9bf0413760b6bd0da49c01e07a
Raveesh Samkaria <rishu9417@gmail.com>
2020-09-10 23:05:12 +0530

    Added : Computed a + b * c

--- compute.sh
+++ compute.sh
@@ -4,3 +4,5 @@ echo ">---< Airthmetic Computation & Sorting >---<"
 read -p "Enter the first value : " a
 read -p "Enter the second value : " b
 read -p "Enter the third value : " c
+w=$(( a + b * c ))
+echo "$a + $b * $c = $w"
